522f61e1418f2b7138a739bb56456c839ef17ad1
DaanCarst <daanbrother@icloud.com>
2023-06-18 16:06:07 +0200

    3 zero states added

--- index.js
+++ index.js
@@ -43,6 +43,64 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/page2', (req, res) => {
+  const url = "https://demofdnd.simplicate.nl/api/v2/crm/organization?limit=11"; 
+  // const url = "https://demofdnd.simplicate.nl/api/v2/crm/person?limit=4";  // Replace with the actual API endpoint URL
+  const headers = {
+    'Authentication-Key': "gS7sibGSth6GQfDkGdLx9AU8T3cj1DoB",
+    'Authentication-Secret': "eiZOT04oWOcQUg2XEzkiI42XyvNInifc"
+  }
+ 
+  // Make the API request using fetch and the headers
+  fetch(url, { headers })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Request failed with status " + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Render the EJS view with the fetched data
+      res.render('page2', { data });
+      const dataArray = data.data;
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the request
+      console.error(error);
+      res.status(500).send('Error occurred');
+    });
+});
+
+app.get('/page3', (req, res) => {
+  const url = "https://demofdnd.simplicate.nl/api/v2/crm/organization?limit=11"; 
+  // const url = "https://demofdnd.simplicate.nl/api/v2/crm/person?limit=4";  // Replace with the actual API endpoint URL
+  const headers = {
+    'Authentication-Key': "gS7sibGSth6GQfDkGdLx9AU8T3cj1DoB",
+    'Authentication-Secret': "eiZOT04oWOcQUg2XEzkiI42XyvNInifc"
+  }
+ 
+  // Make the API request using fetch and the headers
+  fetch(url, { headers })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Request failed with status " + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Render the EJS view with the fetched data
+      res.render('page3', { data });
+      const dataArray = data.data;
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the request
+      console.error(error);
+      res.status(500).send('Error occurred');
+    });
+});
+
 // Stel het poortnummer in waar express op gaat luisteren
 app.set('port', process.env.PORT || 8000)
 

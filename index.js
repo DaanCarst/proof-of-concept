ba6bc5241dc87ac452dc6d34f4eecfc1f2e6204f
Daan Carstens <daanbrother@icloud.com>
2023-06-19 11:14:33 +0200

    showable

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/', (req, res) => {
 
 app.get('/page2', (req, res) => {
   const url = "https://demofdnd.simplicate.nl/api/v2/crm/organization?limit=11"; 
-  const urlbirthday = "https://demofdnd.simplicate.nl/api/v2/crm/person?limit=100";  // Replace with the actual API endpoint URL
+  // const urlbirthday = "https://demofdnd.simplicate.nl/api/v2/crm/person?limit=100";  // Replace with the actual API endpoint URL
   const headers = {
     'Authentication-Key': "gS7sibGSth6GQfDkGdLx9AU8T3cj1DoB",
     'Authentication-Secret': "eiZOT04oWOcQUg2XEzkiI42XyvNInifc"
@@ -67,13 +67,7 @@ app.get('/page2', (req, res) => {
       }
       return response.json();
     })
-    fetch(urlbirthday, { headers })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Request failed with status " + response.status);
-      }
-      return response.json();
-    })
+
 
     .then(data => {
       // Render the EJS view with the fetched data
@@ -90,7 +84,7 @@ app.get('/page2', (req, res) => {
 
 app.get('/page3', (req, res) => {
   const url = "https://demofdnd.simplicate.nl/api/v2/crm/organization?limit=11"; 
-  const urlbirthday = "https://demofdnd.simplicate.nl/api/v2/crm/person?limit=100";  // Replace with the actual API endpoint URL
+  // const urlbirthday = "https://demofdnd.simplicate.nl/api/v2/crm/person?limit=100";  // Replace with the actual API endpoint URL
   const headers = {
     'Authentication-Key': "gS7sibGSth6GQfDkGdLx9AU8T3cj1DoB",
     'Authentication-Secret': "eiZOT04oWOcQUg2XEzkiI42XyvNInifc"
@@ -98,13 +92,6 @@ app.get('/page3', (req, res) => {
  
   // Make the API request using fetch and the headers
   fetch(url, { headers })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Request failed with status " + response.status);
-      }
-      return response.json();
-    })
-    fetch(urlbirthday, { headers })
     .then(response => {
       if (!response.ok) {
         throw new Error("Request failed with status " + response.status);
